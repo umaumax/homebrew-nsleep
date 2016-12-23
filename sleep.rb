4f8f266870ab88a795c59d252a21aebcce5f61b6
Akira Umayabara <umauma.7083@gmail.com>
2016-12-23 19:07:03 +0900

    update file

--- sleep.rb
+++ sleep.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class NSleep < Formula
+class Sleep < Formula
   homepage "https://github.com/umaumax/sleep"
   url "https://github.com/umaumax/sleep/releases/download/0.0.1/nsleep"
   sha256 "758d80a45a3293e3bf6730bde5000f325fc848cd8f3dba5493ef1c3ad84903f4"
