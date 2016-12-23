7a809a55c60afda4fce0e26b0478fefaeac4258d
Akira Umayabara <umauma.7083@gmail.com>
2016-12-23 19:02:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# nsleep command
+
+homebrew tap for nsleep command
+
+[umaumax/sleep]( https://github.com/umaumax/sleep )
+
+This repository maintains the brew tap for "nsleep".
+
+## Installation
+
+```
+$ brew tap umaumax/nsleep
+$ brew install nsleep
+```
+
---
+++ nsleep.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class NSleep < Formula
+  homepage "https://github.com/umaumax/sleep"
+  url "https://github.com/umaumax/sleep/releases/download/0.0.1/nsleep"
+  sha256 "758d80a45a3293e3bf6730bde5000f325fc848cd8f3dba5493ef1c3ad84903f4"
+  head "https://github.com/umaumax/sleep.git"
+  version "0.0.1"
+
+  def install
+    bin.install "nsleep"
+  end
+end
